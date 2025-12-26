572478999ca64d097dff039afc71b67ff3a10bd4
alphacoder-mp3 <shindeakshay223@gmail.com>
2025-12-26 21:35:21 +0530

    Update local search input

--- components/search-in.tsx
+++ components/search-in.tsx
@@ -42,29 +42,29 @@ const sampleData = [
   },
 ];
 
-type SampleDataItem = {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-};
+// type SampleDataItem = {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+//   address: {
+//     street: string;
+//     suite: string;
+//     city: string;
+//     zipcode: string;
+//     geo: {
+//       lat: string;
+//       lng: string;
+//     };
+//   };
+//   phone: string;
+//   website: string;
+//   company: {
+//     name: string;
+//     catchPhrase: string;
+//     bs: string;
+//   };
+// };
 
 const SearchInput = () => {
   /* Implement search input functionality here using sampleData */
